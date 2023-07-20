48991e553a92b933b579cb3daebf1915430fe27c
kader009 <kadermollarasel000@gmail.com>
2023-07-20 18:56:53 +0600

    about and corporate text added

--- src/components/Corporate/Corporate.jsx
+++ src/components/Corporate/Corporate.jsx
@@ -27,15 +27,41 @@ const Corporate = () => {
       </div>
       <div className="culture-box d-flex justify-content-center align-items-center flex-sm-column flex-lg-row">
         <div className="culture-text">
-          <p>Our greatest differentiator is our commitment to delivering value-added IT staffing and managed solutions services, not just temporary help. More specifically, we understand our clients specific needs and evolving business drivers and are dedicated to providing unparalleled value to our clients.</p>
+          <p>promotes innovation, collaboration, and continuous growth. Our culture is built upon the following core values: </p>
+          <span className='fw-bold'>Excellence:</span>
+          <p className='mt-1'>We strive for excellence in everything we do. We set high standards for ourselves 
+          and continuously challenge and inspire each other to achieve remarkable outcomes.</p>
+          <span className='fw-bold'>Innovation:</span>
+          <p className='mt-1'> We embrace innovation as the driving force behind our success. We encourage our 
+          employees to think creatively, explore new ideas, and embrace emerging technologies to provide cutting-edge 
+          automation solutions.</p>
+          <span className='fw-bold'>Collaboration:</span>
+          <p className='mt-1'>We believe in the power of collaboration and teamwork. We foster a supportive and inclusive
+          environment where everyone's ideas are valued and respected. We actively seek diverse perspectives to create
+          the best solutions for our clients.</p>
+          <span className='fw-bold'>Integrity:</span>
+          <p className='mt-1'>We uphold the highest standards of integrity and ethical conduct. We are committed to honesty, 
+          transparency, and maintaining the trust and confidence of our clients, partners, and employees.</p>
+          <span className='fw-bold'>Continuous Learning:</span>
+          <p className='mt-1'>We are passionate about learning and personal growth. We provide opportunities for 
+          professional development, mentorship, and knowledge sharing to enhance the skills and expertise of our employees.</p>
+          <span className='fw-bold'>Customer Focus:</span>
+          <p className='mt-1'>Our clients are at the heart of everything we do. We listen attentively to their needs,
+          understand their challenges, and deliver tailored automation solutions that exceed their expectations.</p>
+          <span className='fw-bold'>Agility:</span>
+          <p className='mt-1'>We embrace change and adapt quickly to new technologies, industry trends, and client requirements. 
+          We foster an agile mindset that enables us to stay ahead of the curve and deliver innovative solutions efficiently.</p>
+          <span className='fw-bold'>Work-Life Balance:</span>
+          <p className='mt-1'>We value the well-being of our employees. We promote a healthy work-life balance, 
+          recognizing that a harmonious life outside of work positively impacts productivity and creativity.</p>
+          <span className='fw-bold'>Social Responsibility:</span>
+          <p className='mt-1'>We are committed to making a positive impact on society and the environment. We actively
+          engage in corporate social responsibility initiatives, promoting sustainability, diversity, and community involvement.</p>
           <br />
-          <p>Having the right number of IT staff - when and where you need them - lets you control costs while maximizing profitability. At XcelTech, we not only deliver the best IT talent around for all types of projects, but also help you develop a strategy for maintaining the necessary IT staff in the short and long-term, so you can focus on savings and performance.</p>
-          <br />
-          <p>we understand the need for quick response to IT staffing demands, and we offer the flexibility and capability to take on assignments lasting from a few weeks to several months or years.</p>
-          <br />
-          <p>XcelTech's regional roots, proven expertise and national prominence allow us to attract top IT professionals and match them to your needs. Our consultants have served diverse industries and are skilled in navigating rapid-development project environments.</p>
-          <br />
-          <p>XcelTech is committed to providing clients with quality, cost-effective IT staffing and managed solutions services that improve profitability, competitiveness and organizational efficiency. We are customer-focused and strive for excellent service, professionalism and fair and honest business practices.</p>
+          <p>This corporate culture statement serves as a guidepost for our actions, decisions, and interactions at EasyWayAutomation. We believe that by embracing 
+          these values, we can create an environment where our employees thrive, our clients succeed, and our At 
+          EasyWayAutomation, we cultivate a vibrant and inclusive corporate culture that organization continues
+          to innovate and grow.</p>
         </div>
         <div className="culture-img ms-4">
           <img src="/Strategy.png" alt="strategy" />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,29 +18,38 @@ const Home = () => {
         <div className="corporate-heading mb-3">about us</div>
       </div>
       <div className="corporate-para ">
-        <p>XCELTECH is a young and fast growing provider of IT Business and ERP Solutions to leading clients in the commercial and government sectors. At XcelTech, we start a flexible partnership between each client, employee and the company. We believe that good employees are well rounded and we empower them to develop a career that includes variety as well as the chance to emphasize specific talents and interests. We also provide the framework for success. We encourage and sponsor training, provide mentoring and conduct performance reviews. Working here means you have access to people and resources that help to make career decisions easier - based on the real-life experience of someone who's likely already been there.</p>
-        <br />
-        <p>XCELTECH partners with its clients to define, implement, and manage a broad range of technologies across multiple platforms, systems, and networks. Our broad geographic presence ensures that a proven and reliable tactical and strategic capability is available and deployable virtually everywhere in North America.</p>
-        <br />
-        <p>At XCELTECH, we are determined to provide the highest level of service to our clients and to partner with them in meeting their goals in:</p>
+        <span className='fw-bold'>Our Team:</span>
+        <p className='mt-1'>Our team consists of highly skilled professionals who are experts in their respective fields. From software engineers and developers to automation specialists, we have a diverse and talented team that is committed to delivering exceptional results. With years of experience and a deep understanding of automation technologies, our team is always at the forefront of industry advancements.</p>
+        <span className='fw-bold'>Our Mission:</span>
+        <p className='mt-1'>Our mission is to empower businesses by simplifying complex processes through automation. We strive to provide our clients with cutting-edge solutions that optimize efficiency, enhance productivity, and unlock new opportunities for growth. By leveraging the latest technologies and best practices, we aim to be the go-to automation partner for businesses worldwide.</p>
+        <span className='fw-bold'>Our Services:</span>
+        <p className='mt-1'>At EasyWayAutomation, we offer a comprehensive range of automation services as well as other tech services tailored to meet the unique needs of our clients our solutions are designed to streamline operations, reduce costs, and drive innovation:</p>
       </div>
       <div className="corporate-bullet">
         <ul style={{ listStyleType: 'none' }}>
-          <li> <img src="/tick.png" alt="tick" className='me-2'/> Enterprise Resource Planning (ERP)</li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Robotic Process Automation (RPA)</li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Business Process Automation</li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Intelligent Data Extraction</li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Machine Learning</li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Project Management</li>
           <li> <img src="/tick.png" alt="tick" className='me-2'/> Application design and development</li>
-          <li> <img src="/tick.png" alt="tick" className='me-2'/> Web Development</li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Web Development
+          </li>
+          <li> <img src="/tick.png" alt="tick" className='me-2'/> Software Development and Engineering</li>
           <li> <img src="/tick.png" alt="tick" className='me-2'/> Business Analysis</li>
           <li> <img src="/tick.png" alt="tick" className='me-2'/> Database Design and Administration</li>
-          <li> <img src="/tick.png" alt="tick" className='me-2'/> Project Management</li>
-          <li> <img src="/tick.png" alt="tick" className='me-2'/> Software Development and Engineering
-</li>
           <li> <img src="/tick.png" alt="tick" className='me-2'/> Systems Analysis</li>
           <li> <img src="/tick.png" alt="tick" className='me-2'/> Quality Assurance</li>
           <li> <img src="/tick.png" alt="tick" className='me-2'/> Technical Writing</li>
           
         </ul>
-      <br />
-      <p className='inner ms-4'>XCELTECH believes in building strong foundation. Key to our success are our Clients, Partners and our Valuable Employees.</p>
+        <br />
+        <span className='fw-bold pt-4 ms-4'>Our Approach:</span>
+        <p className='inner ms-4 mt-1'>We believe in a collaborative approach to automation. We work closely with our clients to understand their specific requirements, challenges, and goals. By combining our expertise with their insights, we develop customized automation solutions that deliver tangible results. Our approach is focused on delivering seamless integration, user-friendly interfaces, and measurable ROI.</p>
+      <span className='fw-bold ms-4'>Conclusion:</span>
+      <p className='inner ms-4 mt-1'>At EasyWayAutomation, we are passionate about leveraging automation to drive success for businesses. With our experienced team, innovative solutions, and client-centric approach, we are committed to helping businesses unlock their full potential. Join us on the path to streamlined operations, increased efficiency, and sustainable growth.
+      We are highly focused on building innovative solutions that address client's most complex digital transformation needs.
+      We deliver on the promise of helping our customers, colleagues and communities thrive in an ever changing world.</p>
       </div>
       </div>
     </div>
